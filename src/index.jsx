b1f7b10be6967a85efeb7f71f1380661e3b3c35b
Željko Brcković <zeljkobrckovic86@gmail.com>
2023-12-28 12:14:33 +0100

    switched to hash router

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Root } from './Root'
-import { BrowserRouter } from 'react-router-dom'
-import '@fontsource-variable/orbitron';
+import { HashRouter } from 'react-router-dom'
+import '@fontsource-variable/orbitron'
 import './styles/main.sass'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/x-realms-counter'>
+    <HashRouter>
       <Root/>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
