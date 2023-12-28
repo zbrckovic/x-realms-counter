a02d9885b51bcd568cc0bb9b6897154f9e6fff7b
Željko Brcković <zeljkobrckovic86@gmail.com>
2023-12-28 12:49:12 +0100

    fixed portrait problem

--- src/components/PortraitMessage/PortraitMessage.jsx
+++ src/components/PortraitMessage/PortraitMessage.jsx
@@ -4,7 +4,7 @@ import styles from './PortraitMessage.module.sass'
 export const PortraitMessage = () => (
   <div className={styles.root}>
     <p>
-      This application is not intended to work in portrait mode. Turn it sideways!
+      This page is not intended to work in portrait mode. Turn it sideways!
     </p>
   </div>
 )
